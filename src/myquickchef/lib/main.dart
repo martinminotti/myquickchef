2e1549396e7a037b7d2249cc7f1565f1de775238
nicoladelbianco <nicoladelbianco97@gmail.com>
2024-03-26 15:23:06 +0100

    create page with bottom navigation bar

--- src/myquickchef/lib/main.dart
+++ src/myquickchef/lib/main.dart
@@ -4,8 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: SafeArea(
-        child: Text("Ciao"),
+        child: Page(),
       ),
     ),
   );
+}
+
+class Page extends StatefulWidget {
+  const Page({super.key});
+
+  @override
+  State<Page> createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Color(0xFF01BAEF),
+        onTap: (index) => setState(() {
+          _currentIndex = index;
+        }),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Preferiti")
+        ],
+      ),
+    );
+  }
 }
